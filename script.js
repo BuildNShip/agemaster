5435a4b40171a4210fc3d6fa31fa422d24956039
shaheenhyderk <shaheenhyderk@gmail.com>
2024-08-30 03:08:32 +0530

    minor change

--- script.js
+++ script.js
@@ -3,33 +3,27 @@ const dobInput = document.getElementById('dob');
 const resultDiv = document.getElementById('result');
 
 flatpickr(dobInput, {
-  dateFormat: 'Y-m-d',
-  maxDate: new Date(),
-  onChange: () => {
-    dobInput.value = moment(dobInput.value).format('YYYY-MM-DD');
-  }
+    dateFormat: 'Y-m-d',
+    maxDate: new Date(),
+    onChange: () => {
+        dobInput.value = moment(dobInput.value).format('YYYY-MM-DD');
+    }
 });
 
 function calculateAge() {
-  const dob = moment(dobInput.value);
-  const now = moment();
-  
-  const seconds = now.diff(dob, 'seconds');
-  const minutes = now.diff(dob, 'minutes');
-  const hours = now.diff(dob, 'hours');
-  const days = now.diff(dob, 'days');
-  const months = now.diff(dob, 'months');
-  const years = now.diff(dob, 'years');
+    const dob = moment(dobInput.value);
+    const now = moment();
 
-  resultDiv.innerHTML = `
+    const minutes = now.diff(dob, 'minutes');
+    const hours = now.diff(dob, 'hours');
+    const days = now.diff(dob, 'days');
+    const months = now.diff(dob, 'months');
+    resultDiv.innerHTML = `
     <div class="result">
-      <p>You have been <span class="or">Alive</span> for:</p>
-      <p><span class="or">${seconds.toLocaleString()}</span> seconds</p>
-      <p><span class="or">${minutes.toLocaleString()}</span> minutes</p>
-      <p><span class="or">${hours.toLocaleString()}</span> hours</p>
-      <p><span class="or">${days.toLocaleString()}</span> days</p>
-      <p><span class="or">${months.toLocaleString()}</span> months</p>
-      <p><span class="or">${years.toLocaleString()}</span> years</p>
+      <p>You lived <span class="or">${minutes.toLocaleString()}</span> minutes</p>
+      <p>You lived <span class="or">${hours.toLocaleString()}</span> hours</p>
+      <p>You lived <span class="or">${days.toLocaleString()}</span> days</p>
+      <p>You lived <span class="or">${months.toLocaleString()}</span> months</p>
     </div>
   `;
 }
